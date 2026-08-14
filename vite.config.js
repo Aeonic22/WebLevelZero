543796aa245726acebfb1c39b9c7fb229854ef35
Aeonic22 <brkic.vjekoslav@gmail.com>
2026-08-14 11:19:42 +0200

    claude vite build, one dist

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/WebLevelZero/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        '00_miniApps/index': '00_miniApps/index.html',
+        '00_miniApps/pages/hello-world/index': '00_miniApps/pages/hello-world/index.html',
+        '00_miniApps/pages/list/index': '00_miniApps/pages/list/index.html',
+        '00_miniApps/pages/todo/index': '00_miniApps/pages/todo/index.html',
+        '00_miniApps/pages/tic-tac-toe/index': '00_miniApps/pages/tic-tac-toe/index.html',
+        '01_cssPlayground/index': '01_cssPlayground/index.html',
+        '02_neda_v01/index': '02_neda_v01/index.html'
+      }
+    }
+  }
+});
